1e11780d72521063d0b9bd59e6b40b91d794869a
Carlos Almeida <caredu_tato@hotmail.com>
2020-09-23 15:28:38 -0500

    changes in main file

--- main.tf
+++ main.tf
@@ -1,19 +1,13 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 resource "aws_instance" "example" {
   ami = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<EOF
-     		 #!/bin/bash
-		 echo "Hollo, World " > index.html
-		 nohup busybox httpd -f -p 8080 &
-		 EOF
+  user_data = file("server_apache.sh")
 
   tags = {
     Name = "terraform-example"
@@ -24,7 +18,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 80
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = var.server_port
     to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
